ec97630209ddaee34edbe455e2911b6e0cfcdb70
Zinawe <kostkost2007@yandex.ru>
2026-03-18 20:15:21 +0300

    end

---
+++ Patch/user_interface.h
@@ -0,0 +1,37 @@
+#ifndef USER_INREFACE
+#define USER_INREFACE
+
+#include <SFML/Graphics.h>
+
+const float WIDTH  = 2000;
+const float HEIGHT = 1200;
+
+const size_t BLUE_TABLET_X_CENTRE = 1411; 
+const size_t BLUE_TABLET_Y_CENTRE = 631; 
+const size_t BLUE_TABLET_RADIUS   = 80; 
+
+const size_t RED_TABLET_X_CENTRE = 642; 
+const size_t RED_TABLET_Y_CENTRE = 560; 
+const size_t RED_TABLET_RADIUS   = 81;
+
+enum SelectedArea
+{
+    BLUE,
+    RED,
+    UNKNOWN
+};
+
+void PlayMelody();
+
+void SetFullScreenBackgroundScale(sfSprite* background_sprite, sfTexture* background_texture, 
+                                  size_t width, size_t height);
+
+sfText* CreateTextObject(const char* message, sfFont* font, sfColor color, int x, int y);
+
+size_t CalcPointsDistance(size_t x_1, size_t y_1, size_t x_2, size_t y_2);
+
+SelectedArea RunUserInterface();
+
+SelectedArea DetectPressButton(size_t x_coord, size_t y_coord);
+
+#endif
